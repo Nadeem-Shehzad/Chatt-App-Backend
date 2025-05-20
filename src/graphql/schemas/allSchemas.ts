bd9269119718c2c7aae2348f33772a8e7dd3f1ab
Nadeem-Shehzad <nadeem.dev@outlook.com>
2025-05-20 22:01:51 +0500

    added contact schema

--- src/graphql/schemas/allSchemas.ts
+++ src/graphql/schemas/allSchemas.ts
@@ -2,10 +2,11 @@ import { gql } from 'graphql-tag';
 
 import { userTypeDefs } from './user/user';
 import { messageTypeDefs } from './message/message';
+import { contactTypeDefs } from './contact/contact';
 
 
 export const typeDefs = gql`
   ${userTypeDefs}
   ${messageTypeDefs}
-
+  ${contactTypeDefs}
 `;
