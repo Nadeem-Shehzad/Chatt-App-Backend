5fdfefe2cd27dc3ca7b1117bb21b2e935dd272c4
Nadeem-Shehzad <nadeem.dev@outlook.com>
2025-05-20 22:03:13 +0500

    added contact types

--- src/utils/customTypes.ts
+++ src/utils/customTypes.ts
@@ -3,67 +3,82 @@ import { Document, Types } from 'mongoose';
 
 
 export interface IUser extends Document {
-    username: string;
-    email: string;
-    password: string;
-    token?: string;
-    contacts: Types.ObjectId[];
+   username: string;
+   email: string;
+   password: string;
+   token?: string;
+   contacts: Types.ObjectId[];
 }
 
 export interface MyContext {
-    userId?: string;
-    email?: string;
+   userId?: string;
+   email?: string;
 }
 
 export interface CustomJwtPayload extends JwtPayload {
-    userId: string;
-    email: string;
-    iat?: number;
-    exp?: number;
+   userId: string;
+   email: string;
+   iat?: number;
+   exp?: number;
 }
 
 export interface AuthResponse {
-    success: boolean;
-    message: string;
-    data: IUser | null
+   success: boolean;
+   message: string;
+   data: IUser | null
 }
 
 export interface SingleUserResponse {
-    success: boolean;
-    message: string;
-    data: IUser | null
+   success: boolean;
+   message: string;
+   data: IUser | null
 }
 
 export interface AllUsersResponse {
-    success: boolean;
-    message: string;
-    data: IUser[] | null
+   success: boolean;
+   message: string;
+   data: IUser[] | null
 }
 
 
 // messages 
 export interface IMessage extends Document {
-    id: Types.ObjectId;
-    sender: Types.ObjectId;
-    receiver: Types.ObjectId;
-    content: string;
-    delivered: Boolean;
+   id: Types.ObjectId;
+   sender: Types.ObjectId;
+   receiver: Types.ObjectId;
+   content: string;
+   delivered: Boolean;
 }
 
 export interface IMessageDTO {
-  id: Types.ObjectId;
-  sender: Types.ObjectId;
-  receiver: Types.ObjectId;
-  content: string;
-  delivered: boolean;
+   id: Types.ObjectId;
+   sender: Types.ObjectId;
+   receiver: Types.ObjectId;
+   content: string;
+   delivered: boolean;
 }
 
 export interface MessageResponse {
-    success: boolean;
-    message: string;
-    data: IMessageDTO  | null
+   success: boolean;
+   message: string;
+   data: IMessageDTO | null
 }
 
 export interface DecodedToken {
-  userId: string;
+   userId: string;
+}
+
+
+// contacts
+export interface IContact extends Document {
+   id: Types.ObjectId;
+   requester: Types.ObjectId;
+   receiver: Types.ObjectId;
+   status: 'pending' | 'accepted' | 'blocked'
+}
+
+export interface ContactResponse {
+   success: boolean;
+   message: string;
+   data: IContact | null
 }
