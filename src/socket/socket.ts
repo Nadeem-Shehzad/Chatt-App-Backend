24e2a5e6ff589f5abaf869084c6f1b8eed9099b2
Nadeem-Shehzad <nadeem.dev@outlook.com>
2025-05-20 12:18:58 +0500

    modify by adding socketauth middleware

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,17 +1,29 @@
-import { Server } from "socket.io";
+import { Server } from 'socket.io';
+import { socketAuth } from '../middlewares/socketAuth';
+
+const userSocketMap = new Map<string, string>(); // userId -> socket.id
 
 const setupSocket = (io: Server): void => {
-   io.on('connection', (socket) => {
-      console.log(`A user Connected with id --> ${socket.id}`);
 
-      socket.on('message', (message) => {
-         io.emit('recieve-messgae', message);
-      });
+   io.use(socketAuth);
+
+   io.on('connection', (socket) => {
+      const userId = (socket as any).userId;
+
+      console.log(`Authenticated user ${userId} connected with socket ${socket.id}`);
+      userSocketMap.set(userId, socket.id);
 
       socket.on('disconnect', () => {
-         console.log(`user --> ${socket.id} is disconnected.`);
+         console.log(`User --> ${socket.id} disconnected.`);
+         for (const [userId, socketId] of userSocketMap.entries()) {
+            if (socketId === socket.id) {
+               userSocketMap.delete(userId);
+               break;
+            }
+         }
       });
    });
-}
+};
 
+export { userSocketMap };
 export default setupSocket;
