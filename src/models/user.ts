295ceb0b0c0cdf3d9f9afec720ed0272678e1839
Nadeem-Shehzad <nadeem.dev@outlook.com>
2025-05-20 22:02:28 +0500

    modify user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -7,7 +7,7 @@ const UserSchema: Schema = new Schema(
       username: { type: String, required: true, trim: true },
       email: { type: String, required: true, unique: true, trim: true },
       password: { type: String, required: true },
-      contacts: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+      //contacts: [{ type: Schema.Types.ObjectId, ref: 'User' }],
       token: { type: String, default: '' }
    },
    { timestamps: true }
