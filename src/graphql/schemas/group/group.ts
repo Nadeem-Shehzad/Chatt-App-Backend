f70044b11e3bcd2c2886de537ffc6490bcae7094
Nadeem-Shehzad <nadeem.dev@outlook.com>
2025-05-22 22:57:08 +0500

    added group schema

---
+++ src/graphql/schemas/group/group.ts
@@ -0,0 +1,16 @@
+import {gql} from 'graphql-tag';
+import { GroupType, GroupResponse } from './types';
+
+export const groupTypeDefs = gql`
+
+ ${GroupType}
+ ${GroupResponse}
+
+ type Mutation{
+    createGroup(name:String!, description: String!): GroupResponse
+    addMemberToGroup(groupId: String!, memberId: String!): GroupResponse
+    removeMemberFromGroup(groupId: String!, memberId: String!): GroupResponse
+    sendMessageToGroup(groupId: String!, content:String!): GroupResponse
+ }
+
+`;
