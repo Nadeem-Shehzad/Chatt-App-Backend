846d25728b7f39c97d7f44ce098a7fd978f1c68a
Nadeem-Shehzad <nadeem.dev@outlook.com>
2025-05-22 15:25:02 +0500

    added getcontacts and getOnlineUsers

--- src/graphql/schemas/contact/contact.ts
+++ src/graphql/schemas/contact/contact.ts
@@ -1,16 +1,41 @@
 import { gql } from 'graphql-tag';
-import { ContactType, ContactResponse } from './types'
+import {
+   ContactType,
+   ContactResponse,
+   ContactsResponse,
+   NContactType,
+   ContactUser,
+   NContactsResponse,
+   OnlineUser,
+   OnlineUsersResponse
+} from './types'
 
 
 export const contactTypeDefs = gql`
 
 ${ContactType}
 ${ContactResponse}
+${ContactsResponse}
+
+# new 
+${ContactUser}
+${NContactType}
+${NContactsResponse}
+
+# online-users
+${OnlineUser}
+${OnlineUsersResponse}
+
+
+ type Query{
+   getContacts: NContactsResponse
+   getOnlineUsers: OnlineUsersResponse
+ }
 
  type Mutation{
-    sendContactRequest(receiverId: ID!): ContactResponse
-    acceptContactRequest(contactId: ID!): ContactResponse
-    blockContact(contactId: ID!): ContactResponse
+   sendContactRequest(receiverId: ID!): ContactResponse
+   acceptContactRequest(contactId: ID!): ContactResponse
+   blockContact(contactId: ID!): ContactResponse
  } 
 
 `;
