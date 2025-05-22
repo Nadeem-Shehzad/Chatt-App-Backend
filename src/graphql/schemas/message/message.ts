c6b31f2117b3e1e05ca3130f1436fd5a813141e8
Nadeem-Shehzad <nadeem.dev@outlook.com>
2025-05-22 15:26:47 +0500

    added user chats related schemas.

--- src/graphql/schemas/message/message.ts
+++ src/graphql/schemas/message/message.ts
@@ -1,19 +1,34 @@
-import {gql} from 'graphql-tag';
-import { MessageTypes, MessageResponse } from './types';
+import { gql } from 'graphql-tag';
+
+import { 
+   MessageTypes, 
+   MessageResponse, 
+   MessagesResponse,
+   UserSummary,
+   ChatSummary,
+   ChatsResponse 
+} from './types';
 
 
 export const messageTypeDefs = gql`
 
  ${MessageTypes}
  ${MessageResponse}
+ ${MessagesResponse}
+
+ ${UserSummary}
+ ${ChatSummary}
+ ${ChatsResponse}
 
 
  type Query{
-   getMessages: String!
+    getMessages(receiverId: String!): MessagesResponse!
+    getAllChats: ChatsResponse
  }
 
  type Mutation{
     sendMessage(receiverId: ID!, content: String!): MessageResponse
+    deleteMessage(messageId: ID!): MessageResponse
  }
 
 `;
