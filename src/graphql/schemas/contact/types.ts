c4330609f14b527e4ca77de88532b12206248117
Nadeem-Shehzad <nadeem.dev@outlook.com>
2025-05-22 15:25:37 +0500

    added getcontacts and getOnlineUsers types

--- src/graphql/schemas/contact/types.ts
+++ src/graphql/schemas/contact/types.ts
@@ -12,7 +12,6 @@ export const ContactType = gql`
 }
 `;
 
-
 export const ContactResponse = gql`
   type ContactResponse {
    success: Boolean!
@@ -20,3 +19,50 @@ export const ContactResponse = gql`
    data: Contact
   }
 `;
+
+export const ContactsResponse = gql`
+  type ContactsResponse {
+   success: Boolean!
+   message: String!
+   data: [Contact]
+  }
+`;
+
+
+export const ContactUser = gql`
+  type ContactUser {
+    _id: ID!
+    username: String!
+    email: String!
+  }
+`;
+
+export const NContactType = gql`
+  type NContact {
+   _id: ID!
+  user: ContactUser!  # Only the other user
+  status: String!
+}
+`;
+
+export const NContactsResponse = gql`
+  type NContactsResponse {
+    success: Boolean!
+    message: String!
+    data: [NContact!]
+}`;
+
+
+export const OnlineUser = gql`
+  type OnlineUser{
+    _id: ID!
+    username: String!
+  } 
+`;
+
+export const OnlineUsersResponse = gql`
+  type OnlineUsersResponse{
+    success: Boolean!
+    message: String!
+    data: [OnlineUser]
+}`;
