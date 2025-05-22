86f20867eb546743ae2d8f7b12be11f6883adbc7
Nadeem-Shehzad <nadeem.dev@outlook.com>
2025-05-22 15:29:48 +0500

    modify for onlineUsers

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,5 +1,7 @@
 import { Server } from 'socket.io';
 import { socketAuth } from '../middlewares/socketAuth';
+import OnlineUser from '../models/onlineUser';
+import { broadcastOnlineUsers } from '../utils/utils';
 
 const userSocketMap = new Map<string, string>(); // userId -> socket.id
 
@@ -7,13 +9,22 @@ const setupSocket = (io: Server): void => {
 
    io.use(socketAuth);
 
-   io.on('connection', (socket) => {
+   io.on('connection', async (socket) => {
       const userId = (socket as any).userId;
 
       console.log(`Authenticated user ${userId} connected with socket ${socket.id}`);
       userSocketMap.set(userId, socket.id);
 
-      socket.on('disconnect', () => {
+      // store in DB
+      await OnlineUser.findOneAndUpdate(
+         { userId },
+         { socketId: socket.id, connectedAt: new Date() },
+         { upsert: true, new: true }
+      );
+
+      broadcastOnlineUsers(io, userSocketMap, userId);
+
+      socket.on('disconnect', async () => {
          console.log(`User --> ${socket.id} disconnected.`);
          for (const [userId, socketId] of userSocketMap.entries()) {
             if (socketId === socket.id) {
@@ -21,6 +32,10 @@ const setupSocket = (io: Server): void => {
                break;
             }
          }
+
+         // remove from DB
+         await OnlineUser.findOneAndDelete({ socketId: socket.id });
+         broadcastOnlineUsers(io, userSocketMap,userId);
       });
    });
 };
