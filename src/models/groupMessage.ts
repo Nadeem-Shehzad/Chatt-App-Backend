9e445a59b3aeb9bcb3e3fda3b7488f0b9b7ed132
Nadeem-Shehzad <nadeem.dev@outlook.com>
2025-06-14 20:13:49 +0500

    updated for messageId

--- src/models/groupMessage.ts
+++ src/models/groupMessage.ts
@@ -5,10 +5,11 @@ import { IGroupMessage } from '../utils/customTypes'
 const groupMessageSchema = new Schema<IGroupMessage>({
     groupId: { type: Schema.Types.ObjectId, ref: 'Group', required: true },
     senderId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    messageId: { type: Schema.Types.ObjectId, required: true, unique: true },
     content: { type: String, required: true },
     seenBy: [{ type: Schema.Types.ObjectId, ref: 'User', default: [] }]
 }, { timestamps: true });
 
 const GroupMessage = mongoose.model<IGroupMessage>('GroupMessage', groupMessageSchema);
 
-export default GroupMessage;
+export default GroupMessage; 
